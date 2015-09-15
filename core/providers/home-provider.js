b8bfba3aafc603c015aaea835a4467f787a02e53
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 15:23:16 +0000

    Add module home-provider

---
+++ core/providers/home-provider.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/providers/home-provider
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class HomeProvider
+ * @extends Montage
+ */
+exports.HomeProvider = Montage.specialize(/** @lends HomeProvider# */ {
+    constructor: {
+        value: function HomeProvider() {
+            this.super();
+        }
+    }
+});

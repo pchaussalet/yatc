6aaf46550e8101f62120f9618c87bc96224b4c04
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 14:00:39 +0000

    Add component card

---
+++ ui/card.reel/card.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/card.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Card
+ * @extends Component
+ */
+exports.Card = Component.specialize(/** @lends Card# */ {
+    constructor: {
+        value: function Card() {
+            this.super();
+        }
+    }
+});

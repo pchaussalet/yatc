bae7e587ffd95f1b903aa767acb5fd6e2b60ae11
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 23:03:21 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -21,7 +21,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     constructor: {
         value: function Main() {
             this._twitterClient = new TwitterClient();
-            location.href = 'https://api.twitter.com/oauth/request_token?oauth_callback=twitterclient://callback';
+      //      location.href = 'https://api.twitter.com/oauth/request_token?oauth_callback=';
             this.homeProvider = new HomeProvider().init(this._twitterClient);
         }
     }

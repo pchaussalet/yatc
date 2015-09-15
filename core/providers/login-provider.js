c84213c0e8d628a90b3c5b215d7352412692b03d
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 23:45:30 +0000

    Update file core/providers/login-provider.js

--- core/providers/login-provider.js
+++ core/providers/login-provider.js
@@ -7,9 +7,31 @@ var Montage = require("montage/core/core").Montage;
  * @extends Montage
  */
 exports.LoginProvider = Montage.specialize(/** @lends LoginProvider# */ {
-    constructor: {
-        value: function LoginProvider() {
-            this.super();
+    _twitterClient: {
+        value: null
+    },
+    
+    init: {
+        value: function(twitterClient) {
+            this._twitterClient = twitterClient;
+        }
+    },
+    
+    getAuthUrl: {
+        value: function() {
+            this._twitterClient.post('https://api.twitter.com/oauth/request_token?oauth_callback=oob')
+            .then(function(response) {
+                console.log('https://api.twitter.com/oauth/authorize?' + response.body);
+            });;
+        }
+    },
+    
+    inputCode: {
+        value: function(code) {
+            this._twitterClient.post('https://api.twitter.com/oauth/access_token?oauth_verifier=' + code)
+            .then(function(data) {
+                console.log(data);
+            });
         }
     }
 });

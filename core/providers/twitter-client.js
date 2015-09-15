38265012d8ba4bbc7bd4df11cb45dca9b3f1b4b7
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 21:43:38 +0000

    Add module twitter-client

---
+++ core/providers/twitter-client.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/providers/twitter-client
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class TwitterClient
+ * @extends Montage
+ */
+exports.TwitterClient = Montage.specialize(/** @lends TwitterClient# */ {
+    constructor: {
+        value: function TwitterClient() {
+            this.super();
+        }
+    }
+});

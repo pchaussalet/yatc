4947005e45c2f154f601e74067167d226972cfc7
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 14:31:47 +0000

    Update component ui/column.reel/column.js

--- ui/column.reel/column.js
+++ ui/column.reel/column.js
@@ -15,7 +15,20 @@ exports.Column = Component.specialize(/** @lends Column# */ {
     
     constructor: {
         value: function Column() {
-            console.log(request);
+            var self = this;
+            this._tweets = [];
+            for (var i = 0; i < 10; i++) {
+                request({
+                    method: 'GET',
+                    url: 'https://baconipsum.com/api/?type=all-meat&sentences=1'
+                }).then(function(data) {
+                    self._tweets.push({
+                        author: 'Foo Bar', 
+                        text: data.substr(0,140}
+                    });
+                });
+            }
+            console.log(this._tweets);
         }
     }
 });

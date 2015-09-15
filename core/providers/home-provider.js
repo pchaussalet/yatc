070d9f90ffd3aed7564d76afbe244ae6ff70776e
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 22:21:48 +0000

    Update file core/providers/home-provider.js

--- core/providers/home-provider.js
+++ core/providers/home-provider.js
@@ -7,8 +7,27 @@ var Montage = require("montage/core/core").Montage;
  * @extends Montage
  */
 exports.HomeProvider = Montage.specialize(/** @lends HomeProvider# */ {
+    _endpoint: {
+        value: '/statuses/home_timeline.json'
+    },
+    
     constructor: {
         value: function HomeProvider() {
+        }
+    },
+    
+    init: {
+        value: function(twitterClient) {
+            this._twitterClient = twitterClient;
+        }
+    },
+    
+    get: {
+        value: function() {
+            return this._twitterClient.get(this._endpointUrl)
+            .then(function(response) {
+                return response.body;
+            });
         }
     }
 });

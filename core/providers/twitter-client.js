1549a4c310c16243978c2935b8a4dce1a1dbba64
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 21:51:54 +0000

    Update files

--- core/providers/twitter-client.js
+++ core/providers/twitter-client.js
@@ -1,14 +1,20 @@
 /**
  * @module core/providers/twitter-client
  */
-var Montage = require("montage/core/core").Montage;
+var Montage = require("montage/core/core").Montage,
+    configuration = require('core/configuration');
 /**
  * @class TwitterClient
  * @extends Montage
  */
 exports.TwitterClient = Montage.specialize(/** @lends TwitterClient# */ {
+    _oauth: {
+        value: null
+    },
+    
     constructor: {
         value: function TwitterClient() {
+            this._oauth = 
         }
     }
 });

c77de82491ccf98fe9beb9c65caf9bd5c5fe81dc
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 15:19:00 +0000

    Add module twitter-controller

---
+++ core/twitter-controller.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/twitter-controller
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class TwitterController
+ * @extends Montage
+ */
+exports.TwitterController = Montage.specialize(/** @lends TwitterController# */ {
+    constructor: {
+        value: function TwitterController() {
+            this.super();
+        }
+    }
+});

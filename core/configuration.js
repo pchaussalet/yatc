93ccb1968e24cc681bb034aa549796264e2db639
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 22:05:55 +0000

    Update files

--- core/configuration.js
+++ core/configuration.js
@@ -1,6 +1,8 @@
 module.exports = {
     twitter: {
-        apiKey:    'wMrDSNjFixqkULPeXJ1PBTBoS',
-        apiSecret: 'H7Xu5pYH54biOUjW9jdFizbsXSmaIQrmpdDghNB9Oc2pstpDkB'
+        apiKey:      'wMrDSNjFixqkULPeXJ1PBTBoS',
+        apiSecret:   'H7Xu5pYH54biOUjW9jdFizbsXSmaIQrmpdDghNB9Oc2pstpDkB',
+        tokenPublic: '45789502-c3EEwJZsqTpG7fikVQlSXgzCYQV0BBzIKXB8tJDLy',
+        tokenSecret: 'SyfD9ocJ3YhxR8bft1g1JImL1UFroIe3enGG1mD7onR5Z'
     }
 }
--- core/providers/twitter-client.js
+++ core/providers/twitter-client.js
@@ -2,6 +2,7 @@
  * @module core/providers/twitter-client
  */
 var Montage = require("montage/core/core").Montage,
+    request = require('montage/core/request'),
     OAuth = require('oauth-1.0a').OAuth,
     configuration = require('core/configuration');
 /**
@@ -9,13 +10,44 @@ var Montage = require("montage/core/core").Montage,
  * @extends Montage
  */
 exports.TwitterClient = Montage.specialize(/** @lends TwitterClient# */ {
+    _serviceUrl: {
+        value: null
+    },
+    
     _oauth: {
         value: null
     },
     
     constructor: {
         value: function TwitterClient() {
-            this._oauth = 
+            this._serviceUrl = 'https://api.twitter.com/1.1';
+            this._oauth = OAuth({
+                consumer: {
+                    public: configuration.twitter.apiKey,
+                    secret: configuration.twitter.apiSecret
+                },
+                signature_method: 'HMAC-SHA1'
+            });
+        }
+    },
+    
+    _request: {
+        value: function(method, path, data) {
+            var requestData = {
+                method: method,
+                url: this._serviceUrl + path,
+                data: data
+            };
+            
+            var token = {
+                public: ''
+            };
+            
+            return request({
+                method: requestData.method,
+                path:   requestData.url,
+                
+            });
         }
     }
 });

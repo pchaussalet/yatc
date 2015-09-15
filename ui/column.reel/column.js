eb31f0caaa56ae8b00b059f05e24f5d42cd44875
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 13:44:18 +0000

    Add component column

---
+++ ui/column.reel/column.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/column.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Column
+ * @extends Component
+ */
+exports.Column = Component.specialize(/** @lends Column# */ {
+    constructor: {
+        value: function Column() {
+            this.super();
+        }
+    }
+});

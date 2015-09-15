26f1f0c57f73b455935c39a082fd9c4fd4dd3d02
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-15 15:21:34 +0000

    Add module column-controller

---
+++ core/column-controller.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/column-controller
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class ColumnController
+ * @extends Montage
+ */
+exports.ColumnController = Montage.specialize(/** @lends ColumnController# */ {
+    constructor: {
+        value: function ColumnController() {
+            this.super();
+        }
+    }
+});
